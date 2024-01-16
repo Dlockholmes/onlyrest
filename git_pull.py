c89eb1f5a4adb9e34e616e53e24ae409bcac5861
ignisytb <ignisytb@dgist.ac.kr>
2024-01-16 17:29:16 +0900

    git pull changed

--- git_pull.py
+++ git_pull.py
@@ -2,8 +2,8 @@ import os, schedule, time
 
 def job():
     os.system("git pull origin main")
-    os.system("cp -r /home/ignisytb/rest_git/app /home/ignisytb/rest/app")
-    os.system("cp -r /home/ignisytb/rest_git/wsgi /home/ignisytb/rest/wsgi")
+    os.system("cp -r /home/ignisytb/rest_git/app/* /home/ignisytb/rest/app/")
+    os.system("cp -r /home/ignisytb/rest_git/wsgi/* /home/ignisytb/rest/wsgi/")
     os.system("git add .")
     os.system("git commit -m '[server]'")
     os.system("git push origin main")
