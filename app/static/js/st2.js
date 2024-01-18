81286bae2064c300b2469305363d36cbf138ebc4
ignisytb <ignisytb@dgist.ac.kr>
2024-01-18 16:20:25 +0900

    unicode test

--- app/static/js/st2.js
+++ app/static/js/st2.js
@@ -45,7 +45,7 @@ function _onload() {
 
 function _arrowclick(e){
     if(e.target.id!=document.getElementsByClassName("arrow").length) {
-        e.target.style.color="white";
+        e.target.style.color="#101010";
         document.getElementById((parseInt(e.target.id) + 1).toString()).addEventListener("click", _arrowclick);
     } else {
         e.target.style.color="yellow";
