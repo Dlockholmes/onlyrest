997872ce6a0c2c5352d7a31cf651eafa1a1ffeb7
ignisytb <ignisytb@dgist.ac.kr>
2024-03-19 16:18:27 +0900

    wordle

--- app/app.py
+++ app/app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, session, redirect
 import json, re, pymysql, random, string
 from datetime import datetime
 
-app = Flask(__name__)
+app = Flask(__name__,static_url_path="/")
 app.secret_key = "hello world"
 
 freepass=["d'lock","unlock"]
